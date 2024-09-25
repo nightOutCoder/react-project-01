16385d6e63a17972eb18b1b41c7ed81cfc54869b
NightOutCoderAtGit <notalkonlycode@gmail.com>
2024-09-26 00:40:14 +0530

    added function

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Images(){
+  return (
+    <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src={viteLogo} className="logo" alt="Vite logo" />
+    </a>
+    <a href="https://react.dev" target="_blank">
+      <img src={reactLogo} className="logo react" alt="React logo" />
+    </a>
+  </div>
+  );
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Images/>
       <h1>Vite + React !!!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
